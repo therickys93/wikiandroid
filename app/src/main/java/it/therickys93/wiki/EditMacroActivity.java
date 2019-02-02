cb3acbbe287fae874ba6da1566aed1e86976bb8a
Riccardo Crippa <therickys93@gmail.com>
2019-02-02 20:45:59 +0100

    wip creazione edit macro activity

---
+++ app/src/main/java/it/therickys93/wiki/EditMacroActivity.java
@@ -0,0 +1,13 @@
+package it.therickys93.wiki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditMacroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_macro);
+    }
+}
